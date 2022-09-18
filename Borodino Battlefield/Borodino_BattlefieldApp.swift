bd4bbee19ea73312c655587b12b3f733868c7757
Alexander <a.pride.xxi@tutanota.com>
2022-09-18 20:17:35 +0300

    Initial commit

---
+++ Borodino Battlefield/Borodino_BattlefieldApp.swift
@@ -0,0 +1,17 @@
+//
+//  Borodino_BattlefieldApp.swift
+//  Borodino Battlefield
+//
+//  Created by Александр Прайд on 18.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct Borodino_BattlefieldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Borodino Battlefield/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Borodino Battlefield
+//
+//  Created by Александр Прайд on 18.09.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
