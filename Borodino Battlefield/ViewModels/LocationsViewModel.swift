d5d68d93f8277312d76153ceb5d522381e36b6d9
Alexander <a.pride.xxi@tutanota.com>
2022-09-19 12:27:26 +0300

    Add map

--- Borodino Battlefield/ViewModels/LocationsViewModel.swift
+++ Borodino Battlefield/ViewModels/LocationsViewModel.swift
@@ -6,13 +6,37 @@
 //
 
 import Foundation
+import MapKit
+import SwiftUI
 
 class LocationsViewModel: ObservableObject {
     
+    // All loaded locations
     @Published var locations: [Location]
+    
+    // Current location on map
+    @Published var mapLocation: Location {
+        didSet {
+            updateMapRegion(location: mapLocation)
+        }
+    }
+    
+    @Published var mapRegion: MKCoordinateRegion = MKCoordinateRegion()
+    let mapSpen = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
     
     init() {
         let locations = LocationDataService.locations
         self.locations = locations
+        self.mapLocation = locations.first!
+        
+        self.updateMapRegion(location: locations.first!)
+    }
+    
+    private func updateMapRegion(location: Location) {
+        withAnimation {
+            mapRegion = MKCoordinateRegion(
+                center: location.coordinates,
+                span: mapSpen)
+        }
     }
 }
